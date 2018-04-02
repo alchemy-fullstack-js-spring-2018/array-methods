87b6779e4cfb941ffcaad4b1052dce1dc9bd7ae8
Charlie <heinercj@gmail.com>
2018-04-02 15:44:38 -0700

    filter test passes with filter function

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,10 +1,17 @@
 const assert = require('assert');
 const map = require('../lib/map');
+const filter = require('../lib/filter');
 
 describe('array methods', () => {
     it('map returns array of elements modified by callback', () => {
         const arr = [1, 2, 3];
         const result = map(arr, x => x + 1);
         assert.deepEqual(result, [2, 3, 4]);
+    });
+
+    it('filter returns array of elements that return true from callback', () => {
+        const arr = [1, 2, 3];
+        const result = filter(arr, x => x > 2);
+        assert.deepEqual(result, [3]);
     });
 });
