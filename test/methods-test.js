ee2354af293d46318905245eb65aaa0e4f20a7d6
Ryan Manro <rmanro@gmail.com>
2018-04-02 16:32:13 -0700

    FIND_INDEX method for array that has no even number testing and functioning

--- test/methods-test.js
+++ test/methods-test.js
@@ -1,6 +1,10 @@
 const assert = require('assert');
 const methods = require('../lib/methods');
 
+//callback function to determine if number is even
+const evenNum = x => x % 2 === 0;
+
+
 it('MAP - Returns a new array of each number squared', () => {
     const arr = [1, 2, 3];
     const squareNum = x => x * x;
@@ -10,14 +14,18 @@ it('MAP - Returns a new array of each number squared', () => {
 
 it('FILTER - Returns a new array where the numbers are even', () => {
     const arr = [1, 2, 3, 4];
-    const evenNum = x => x % 2 === 0;
     const evenArray = methods.filter(arr, evenNum);
     assert.deepEqual(evenArray, [2, 4]);
 });
 
 it('FIND_INDEX - Returns index of first number that is even', () => {
     const arr = [1, 2, 3, 4];
-    const evenNum = x => x % 2 === 0;
     const evenIndex = methods.findIndex(arr, evenNum);
     assert.equal(evenIndex, 1);
+});
+
+it('FIND_INDEX - Returns -1 since the array has no even number', () => {
+    const arr = [1, 3, 5, 7];
+    const noEvenIndex = methods.findIndex(arr, evenNum);
+    assert.equal(noEvenIndex, -1);
 });
