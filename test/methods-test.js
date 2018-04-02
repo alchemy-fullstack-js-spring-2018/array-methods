e0550bbdbc8c2cb44618d62bf4401514ebe7902b
Ryan Manro <rmanro@gmail.com>
2018-04-02 16:10:02 -0700

    FILTER methods tests and functions

--- test/methods-test.js
+++ test/methods-test.js
@@ -7,3 +7,10 @@ it('MAP - Returns a new array of each number squared', () => {
     const squaredArray = methods.map(arr, squareNum);
     assert.deepEqual(squaredArray, [1, 4, 9]);
 });
+
+it('FILTER - Returns a new array where the numbers are even', () => {
+    const arr = [1, 2, 3, 4];
+    const evenNum = x => x % 2 === 0;
+    const evenArray = methods.filter(arr, evenNum);
+    assert.deepEqual(evenArray, [2, 4]);
+});
