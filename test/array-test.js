2f0889270ff607f647eba3138b1a2bccc8fdfa66
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-02 16:34:46 -0700

    added three arrays, two in test and one in array.js

--- test/array-test.js
+++ test/array-test.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
 const array = require('../lib/array');
 
-describe('array-methods', () =>{
+describe('array-methods', () => {
 
     it('map', () => {
+    
+        const testArray = [2, 3, ,6, 5];
+        // const mapArray = array.map(testArray)
+        const mappedArray = testArray.map(x => x * 2);
+        
 
     })
 
