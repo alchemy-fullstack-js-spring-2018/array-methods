8f2f43d5d2262e327df58ccf149c3006cef86038
victorbofill <victorm.bofill@gmail.com>
2018-04-02 16:40:03 -0700

    got map working

--- test/map.test.js
+++ test/map.test.js
@@ -3,10 +3,11 @@ const arrayMethods = require('../lib/map');
 
 describe('array methods', () => {
     it('map', () => {
-        const testArray = arrayMethods.map([1, 2, 3, 4], (n) => {
-            n * 2;
-        });
+        const testArray = arrayMethods.myMap(
+            [1, 2, 3, 4],
+            (n) => {return n * 2;}
+        );
 
-        assert.equal(testArray, [2, 4, 6, 8]);
+        assert.deepEqual(testArray, [2, 4, 6, 8]);
     });
 });
