83b82e44e287997af68e34b2917dfcfc09239739
CharlyWelch <blwbiology@gmail.com>
2018-04-02 16:26:28 -0700

    add test and function for map

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -3,7 +3,10 @@ const func = require('../lib/array-methods');
 
 describe('Functional array methods', () => {
 
-    it('', () => {
+    const array = [1, 2, 3, 4, 5];
 
+    it('maps an array given a callback', () => {
+        const mapped = func.map(array, x => x + 1);
+        assert.deepEqual(mapped, [2, 3, 4, 5, 6]);
     });
 });
