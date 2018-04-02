5c4d2598de8343f62c6d90005194aa3278a4b4fe
Charlie <heinercj@gmail.com>
2018-04-02 16:28:38 -0700

    findIndex funtion returns -1 if no truthy value found

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const map = require('../lib/map');
 const filter = require('../lib/filter');
+const findIndex = require('../lib/findIndex');
 
 describe('array methods', () => {
     it('map returns array of elements modified by callback', () => {
@@ -15,9 +16,26 @@ describe('array methods', () => {
         assert.deepEqual(result, [3]);
     });
 
-    it('find index returns item index that has truthy value of callback', () => {
-        const arr = [1, 2, 3];
+    it('findIndex returns fist item index that has truthy value of callback', () => {
+        const arr = [1, 2, 3, 2];
         const result = findIndex(arr, x => x === 2);
-        assert.deepEqual(result, [1]);
+        assert.deepEqual(result, 1);  
     });
+
+    it('findIndex returns -1 if no truthy value found', () => {
+        const arr = [1, 2, 3];
+        const result = findIndex(arr, x => x === 10);
+        assert.equal(result, -1);
+    });
+
+    // it('findIndex ignores empty spaces in array', () => {
+    //     let callCount = 0;
+    //     const callback = x => x === 3;
+    //     const arr = [1, ,2, 3];
+    //     const result = findIndex(arr, x => {
+    //         callCount ++;
+    //         callback(x);
+    //     });
+    //     assert.deepEqual(callCount, 3);
+    // });
 });
