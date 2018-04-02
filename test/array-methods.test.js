6b9aadf6deffa74ae5334de88721bb65e533ef70
Charlie <heinercj@gmail.com>
2018-04-02 16:46:56 -0700

    every method passes test for true and false scenario

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const map = require('../lib/map');
 const filter = require('../lib/filter');
 const findIndex = require('../lib/findIndex');
+const every = require('../lib/every');
 
 describe('array methods', () => {
     it('map returns array of elements modified by callback', () => {
@@ -27,6 +28,23 @@ describe('array methods', () => {
         const result = findIndex(arr, x => x === 10);
         assert.equal(result, -1);
     });
+
+    it('every returns true if every item in array is truthy', () => {
+        const arr = [1, 2, 3];
+        const result = every(arr, x => x < 5);
+        assert.equal(result, true);
+    });
+
+    it('every returns false if falsy item in array', () => {
+        const arr = [1, 2, 3];
+        const result = every(arr, x => x === 1);
+        assert.equal(result, false);
+    });
+
+    // it('reduce takes a callback and arr and returns single value', () => {
+    //     const arr = [1, 2, 3];
+    //     const result = reduce(arr, (acc, curr) => acc + curr);
+    // });
 
     // it('findIndex ignores empty spaces in array', () => {
     //     let callCount = 0;
