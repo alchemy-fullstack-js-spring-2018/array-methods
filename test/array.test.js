05056af626c2c0c724f4e770acd9e7c4a687a73a
Jack Lomax <lomax15@me.com>
2018-04-03 11:22:39 -0700

    functional every method and test works

--- test/array.test.js
+++ test/array.test.js
@@ -2,16 +2,20 @@ const assert = require('assert');
 const map = require('../lib/map');
 const findIndex = require('../lib/findIndex');
 const filter = require('../lib/filter');
+const every = require('../lib/every');
+// const reduce = require('../lib/reduce');
 
 describe('array methods', () => {
     it('map', () => {
         const array = [0, 1, 2, 3];
+
         const mapped = map(array, x => x + 1);
         assert.deepEqual(mapped, [1, 2, 3, 4]);
     });
 
     it('findIndex', () => {
         const array = [0, 1, 2, 3];
+
         const result = findIndex(array, (item) => {
             if(item === 1) {
                 return true;
@@ -27,4 +31,27 @@ describe('array methods', () => {
 
         assert.deepEqual(filtered, [3]);
     });
+
+    it('every', () => {
+        const array = [0, 1, 2, 3];
+
+        const result = every(array, (item) => {
+            if(item > 0){
+                return true;
+            } else {
+                return false;
+            }
+        });
+        assert.equal(result, false);
+    });
+
+    // it('reduce', () => {
+    //     const array = [0, 1, 2, 3];
+
+    //     const result = reduce(array, (sum, num)  => {
+    //         return sum + num;
+    //     });
+        
+    //     assert.equal(result, 15);
+    // });
 });
