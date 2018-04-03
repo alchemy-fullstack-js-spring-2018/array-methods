7bfb6007a29f3fe3294ff3c3c0cc4bd3c341441b
Carmen Perezchica <crperezchica@gmail.com>
2018-04-03 11:01:08 -0700

    filter array test passed

--- test/array.test.js
+++ test/array.test.js
@@ -11,8 +11,41 @@ describe('arrayMethods', () =>{
     it('return only even numbers', ()=> {
         const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         const filterA = arrayMethods.filter(myArray, x => x % 2 == 0);
-        assert.equal(filterA, [2, 4, 6, 8, 10]);
+        assert.deepEqual(filterA, [2, 4, 6, 8, 10]);
     });
     
+    it('returns index of element', () => {
+        const array = [4, 5, 8];
+        const result = arrayMethods.findIndex(array, (item) => {
+            if(item === 5){
+                return true;
+            }
+        });
+        assert.equal(result, 1);
+    });
+
+    it('reduce to singal value', () => {
+        const array = [1, 2, 3, 4];
+        const result = reduce(array, (sum, num, i) => {
+            return sum + num;
+        });
+        assert.equal(result, 10);
+    });
+
+    it('every', () => {
+        const array = [5, 7, 3, 4];
+
+        const result = every(array, (item) => {
+           
+            if(item > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        assert.equal(result, true);
+    });
+
 });
 
