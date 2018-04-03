05f450a1b47726b211820522c6fca9faeae9c8c6
Carmen Perezchica <crperezchica@gmail.com>
2018-04-03 07:24:09 -0700

    filter array is finally producing a test results, just the opposite.

--- test/array.test.js
+++ test/array.test.js
@@ -1,13 +1,18 @@
 const assert = require('assert');
 const arrayMethods = require('../lib/arrayMethods');
 
-describe('map', () =>{
-    it('returns true if elements match', ()=>{
+describe('arrayMethods', () =>{
+    it('return true if new array', ()=>{
         const array = [2, 4, 6];
         const mapA = arrayMethods.map(array, x => x * 2);
-        assert.deepEqual(mapA,[4,8,12]);
+        assert.deepEqual(mapA, [4, 8, 12]);
     });
 
+    it('return only even numbers', ()=> {
+        const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const filterA = arrayMethods.filter(myArray, x => x % 2 == 0);
+        assert.equal(filterA, [2, 4, 6, 8, 10]);
+    });
     
-})
+});
 
