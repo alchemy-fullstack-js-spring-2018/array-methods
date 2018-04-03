3042540e77c372eab70b14ab2b675b01ff03989c
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-02 20:23:28 -0700

    reduce function uses inital value as expected

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -8,5 +8,11 @@ describe('reduce', () => {
     it('adds numerical elements of an array when an initial value of zero is provided', () => {
         const arraySum = reduce(originalArray, callback, 0);
         assert.equal(arraySum, 6);
-    }); 
+    });
+
+    it('adds numerical elements of an array to an initial value', () => {
+        const arraySum = reduce(originalArray, callback, 100);
+        assert.equal(arraySum, 106);
+    });
+ 
 });
