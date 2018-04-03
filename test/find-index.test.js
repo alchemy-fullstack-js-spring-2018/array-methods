60ed09a2ba11560b48a57ab6c72a39a53c42c24e
Steele Walston <steele.walston@gmail.com>
2018-04-03 09:32:30 -0700

    find index tests work

---
+++ test/find-index.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const every = require('../lib/find-index');
+
+describe('findIndex', () => {
+    const isEven = x => x % 2 === 0;
+    it('returns index of first true value', () => {
+        const arr = [3, 2, 3, 3];
+        const anyEven = every(arr, isEven);
+        assert.equal(anyEven, 1);
+    }),
+    it('returns false if no index is true', () => {
+        const arr = [3, 3, 3, 3];
+        const anyEven = every(arr, isEven);
+        assert.equal(anyEven, false);
+    })
+    ;
+});
