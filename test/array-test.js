becbd03ea85ad86c345cfa364853f854ca99ed57
Tasha <tashazun@gmail.com>
2018-04-02 17:51:46 -0700

    filter pass

--- test/array-test.js
+++ test/array-test.js
@@ -9,4 +9,10 @@ describe('array methods', () => {
         const newArr = array.map(arr, x => x * x);
         assert.deepEqual(newArr,[1, 4, 9, 16]);
     })
+
+    it('filter', () => {
+        const arr = [1, 2, 3, 4];
+        const filtered = array.filter(arr, x => x > 2);
+        assert.deepEqual(filtered, [3, 4]);
+    })
 }) 
