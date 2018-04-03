f6ef22e087debc873adffac0ffdd8255aa92f746
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-02 17:26:39 -0700

    map function processes elements as expected

--- test/map.test.js
+++ test/map.test.js
@@ -15,11 +15,19 @@ describe('map', () => {
         const arrayLengthTest = originalArray.length === oneAddedArray.length;
         assert.equal(arrayLengthTest, true);
     });
+
+    it('processes each element in the original array as expected', () => {
+        assert.deepEqual(oneAddedArray, [2, 3, 4, 5, 6]);
+    });
+    
+    it('processes each element in the original array as expected', () => {
+        const squaredArray = map(originalArray, x => x * x);
+        assert.deepEqual(squaredArray, [1, 4, 9, 16, 25]);
+    });
     
     it('returns a new array', () => {
         const noChangeArray = map(originalArray, x => x);
         const newArrayTest = originalArray === noChangeArray;
         assert.equal(newArrayTest, false);
-    });
-    
+    });    
 });
