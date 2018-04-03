98ac1b726580580600a6a06fd8937af327f8aafb
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-02 17:16:36 -0700

    map function returns a new array

--- test/map.test.js
+++ test/map.test.js
@@ -4,15 +4,22 @@ const map = require('../lib/map');
 describe('map', () => {
     const addOne = x => x + 1;
     const originalArray = [1, 2, 3, 4, 5];
-    const newArray = map(originalArray, addOne);
+    const oneAddedArray = map(originalArray, addOne);
 
     it('returns an array', () => {
-        const arrayTest = Array.isArray(newArray);
+        const arrayTest = Array.isArray(oneAddedArray);
         assert.equal(arrayTest, true);
     });
 
     it('returns an array of the same length', () => {
-        const arrayLengthTest = originalArray.length === newArray.length;
+        const arrayLengthTest = originalArray.length === oneAddedArray.length;
         assert.equal(arrayLengthTest, true);
     });
+    
+    it('returns a new array', () => {
+        const noChangeArray = map(originalArray, x => x);
+        const newArrayTest = originalArray === noChangeArray;
+        assert.equal(newArrayTest, false);
+    });
+    
 });
