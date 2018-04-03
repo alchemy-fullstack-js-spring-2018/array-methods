5900862ccecf43e82a2bc8f4ed0d298552a23a0e
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-03 12:05:39 -0700

    finally figuring out how to write a test

---
+++ test/array-test.js
@@ -0,0 +1,9 @@
+const assert = require ('assert');
+const array = require ('../lib/array');
+
+
+it ('creates new array of same length', () => {
+    let array = ['banana', 'orange', 'pineapple'];
+    let newArray = ['banana', 'apple', 'cherry'];
+    assert.equal(newArray, true);
+});
