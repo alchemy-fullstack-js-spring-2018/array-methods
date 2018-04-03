99755ceefd033fac5a5da57c5cb22df18403b853
Jack Lomax <lomax15@me.com>
2018-04-03 10:43:52 -0700

    functional findIndex and test

--- test/array.test.js
+++ test/array.test.js
@@ -1,10 +1,21 @@
 const assert = require('assert');
-const map = require('../lib/methods');
+const map = require('../lib/map');
+const findIndex = require('../lib/findIndex');
 
 describe('array methods', () => {
     it('map', () => {
         const array = [0, 1, 2, 3];
         const mapped = map(array, x => x + 1);
         assert.deepEqual(mapped, [1, 2, 3, 4]);
+    });
+
+    it('findIndex', () => {
+        const array = [0, 1, 2, 3];
+        const result = findIndex(array, (item) => {
+            if(item === 1) {
+                return true;
+            }
+        });
+        assert.equal(result, 1);
     });
 });
