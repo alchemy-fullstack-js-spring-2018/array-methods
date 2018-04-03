87adea4287c43a6c5e3d37477e4fa245d4e37b47
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-02 20:18:53 -0700

    reduce function accomplishes basic addition

---
+++ test/reduce.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const reduce = require('../lib/reduce');
+
+describe('reduce', () => {
+    const originalArray = [1, 2, 3];
+    const callback = (accumulator, x) => accumulator + x;
+
+    it('adds numerical elements of an array when an initial value of zero is provided', () => {
+        const arraySum = reduce(originalArray, callback, 0);
+        assert.equal(arraySum, 6);
+    }); 
+});
