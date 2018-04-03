0a53273b07c2f19d64082c77c1d22211700201ab
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-02 20:47:08 -0700

    reduce function does more than add

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -2,21 +2,27 @@ const assert = require('assert');
 const reduce = require('../lib/reduce');
 
 describe('reduce', () => {
-    const originalArray = [1, 2, 3];
-    const callback = (accumulator, x) => accumulator + x;
+    const originalArray = [1, 2, 3, 4, 5];
+    const gatherSum = (accumulator, x) => accumulator + x;
 
     it('adds numerical elements of an array when an initial value of zero is provided', () => {
-        const arraySum = reduce(originalArray, callback, 0);
-        assert.equal(arraySum, 6);
+        const arraySum = reduce(originalArray, gatherSum, 0);
+        assert.equal(arraySum, 15);
     });
 
     it('adds numerical elements of an array to an initial value', () => {
-        const arraySum = reduce(originalArray, callback, 100);
-        assert.equal(arraySum, 106);
+        const arraySumPlusInitial = reduce(originalArray, gatherSum, 100);
+        assert.equal(arraySumPlusInitial, 115);
     });
  
     it('adds numerical elements of an array when no initial value is provided', () => {
-        const arraySum = reduce(originalArray, callback);
-        assert.equal(arraySum, 6);
-    }); 
+        const arraySumWithoutInitial = reduce(originalArray, gatherSum);
+        assert.equal(arraySumWithoutInitial, 15);
+    });
+    
+    it('does more than addition', () => {
+        const findHighest = (a, b) => a > b ? a : b;
+        const highest = reduce(originalArray, findHighest);
+        assert.equal(highest, 5);
+    });
 });
