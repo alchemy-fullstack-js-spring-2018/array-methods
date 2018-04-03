8b72b0216c42b2b0d4b742aacf1de8de6ec95002
victorbofill <victorm.bofill@gmail.com>
2018-04-03 10:24:40 -0700

    added more tests

--- test/every-test.js
+++ test/every-test.js
@@ -1,11 +1,20 @@
 const assert = require('assert');
 const arrayMethods = require('../lib/every');
 
-describe('array methods', () => {
-    it('every', () => {
+describe('EVERY', () => {
+    it('every: testing evens', () => {
         const result = arrayMethods.myEvery(
             [2, 4, 6, 8],
             (n) => (n % 2) === 0
+        );
+
+        assert.equal(result, true);
+    });
+
+    it('every: testing odds', () => {
+        const result = arrayMethods.myEvery(
+            [1, 3, 5, 7],
+            (n) => (n % 2) !== 0
         );
 
         assert.equal(result, true);
--- test/map.test.js
+++ test/map.test.js
@@ -1,13 +1,22 @@
 const assert = require('assert');
 const arrayMethods = require('../lib/map');
 
-describe('array methods', () => {
-    it('map', () => {
+describe('MAP', () => {
+    it('map: multiplication', () => {
         const testArray = arrayMethods.myMap(
             [1, 2, 3, 4],
             (n) => n * 2
         );
 
         assert.deepEqual(testArray, [2, 4, 6, 8]);
+    });
+
+    it('map: addition', () => {
+        const testArray = arrayMethods.myMap(
+            [1, 2, 3, 4],
+            (n) => n + 2
+        );
+
+        assert.deepEqual(testArray, [3, 4, 5, 6]);
     });
 });
