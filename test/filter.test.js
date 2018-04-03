cefdeefe2493df924f022b856276aea8a338ef40
victorbofill <victorm.bofill@gmail.com>
2018-04-02 17:16:26 -0700

    finished filter and removed "push"

--- test/filter.test.js
+++ test/filter.test.js
@@ -2,5 +2,12 @@ const assert = require('assert');
 const arrayMethods = require('../lib/filter');
 
 describe('array methods', () => {
-    
-})
+    it('filter', () => {
+        const testArray = arrayMethods.myFilter(
+            [1, 2, 3, 4, 5, 6, 7, 8],
+            (n) => {return (n % 2) === 0;}
+        );
+
+        assert.deepEqual(testArray, [2, 4, 6, 8]);
+    });
+});
