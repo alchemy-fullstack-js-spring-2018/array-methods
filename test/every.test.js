5c33292829395a700c6499dcc29580b6610a5552
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-03 11:16:30 -0700

    every function returns false as expected

---
+++ test/every.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const every = require('../lib/every');
+
+describe('every', () => {
+    const array = [0, 1, 2, 3, 4, 5];
+
+    it('returns false if the callback for any element returns false', () => {
+        const notEvery = every(array, x => x % 2);
+        assert.equal(notEvery, false);
+    });
+});
