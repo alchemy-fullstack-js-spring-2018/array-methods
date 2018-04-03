620f49954858f206f2acb66a88371ce325e4da7a
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-02 17:53:25 -0700

    add return -1 if no item is found for findIndex

--- test/find-index.test.js
+++ test/find-index.test.js
@@ -3,11 +3,18 @@ const findIndex = require('../lib/find-index');
 
 describe('find-index', () => {
 
-    it('finds index of number that equals 8', () => {
+    it('finds index of item that equals 8', () => {
         const equalsEight = x => x === 8;
         const arr = [3, 4, 5, 8, 10, 8, 12];
         const indexNumber = findIndex(arr, equalsEight);
         
-        assert.deepEqual(indexNumber, 3);
+        assert.equal(indexNumber, 3);
+    });
+    it('returns -1 if no item is found', () => {
+        const equalsSeven = x => x === 7;
+        const arr = [3, 4, 5, 8, 10, 8, 12];
+        const indexNumber = findIndex(arr, equalsSeven);
+
+        assert.equal(indexNumber, -1);
     });
 });
