1b8b51a38d1161743574443dac03220ebbd2860c
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-09 01:39:25 -0700

    second test and function

--- test/array-test.js
+++ test/array-test.js
@@ -2,9 +2,15 @@ const assert = require ('assert');
 const array = require ('../lib/array');
 
 
-
-it ('creates new array of same length', () => {
-    const arr = ['banana', 'orange', 'pineapple'];
+//test to map new array
+it ('creates new array of same length (maps)', () => {
+    const array = ['banana', 'orange', 'pineapple'];
     const newArray = array.map(arr, x => x * 2);
     assert.deepEqual(newArray, true);
+});
+//test to filter new array
+it ('filters new array', () => {
+    const array = ['cherry', 'blueberry', 'kiwi', 'banana'];
+    const newArray = array.filter(array, x => x.length < array.length);
+    assert.deepEqual(newArray, ['banana']);
 });
